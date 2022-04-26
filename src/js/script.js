92a9550016f5bf5b2c7546ce18d006edca861e41
gabinpoa <gabrielnpoa@gmail.com>
2022-04-26 17:13:24 -0300

    Using spread in the values array to exclude the forEach function

--- src/js/script.js
+++ src/js/script.js
@@ -10,20 +10,18 @@ document.body.append(calculatorContainer);
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const operators = ["*", "/", "+", "-", "."];
 const specialValues = ["C", "AC", "="];
-const values = [operators, numbers, specialValues];
+const values = [...operators, ...numbers, ...specialValues];
 
-values.forEach((valueArray) => {
-  valueArray.forEach((value) => {
-    let button = document.createElement("button");
-    if ((value === "=") | (value === "AC")) {
-      button.classList = "span2";
-    }
-    button.type = "button";
-    button.innerText = value;
-    calculatorContainer.append(button);
-    button.addEventListener("click", () => {
-      addToDisplay(value);
-    });
+values.forEach((value) => {
+  let button = document.createElement("button");
+  if ((value === "=") | (value === "AC")) {
+    button.classList = "span2";
+  }
+  button.type = "button";
+  button.innerText = value;
+  calculatorContainer.append(button);
+  button.addEventListener("click", () => {
+    addToDisplay(value);
   });
 });
 
